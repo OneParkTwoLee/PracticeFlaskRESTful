92f014468cf2bdce68ec7a32941aa3fc7cb90698
EastFull <huromech@gmail.com>
2020-08-01 15:41:31 +0900

    modify label -> size, remove file field

--- file_crud_restful/api/models.py
+++ file_crud_restful/api/models.py
@@ -1,7 +1,6 @@
 class FileModel:
-    def __init__(self, file, creation_date, label):
+    def __init__(self, creation_date, size):
         # We will automatically generate the new id
         self.id = 0
-        self.file = file
         self.creation_date = creation_date
-        self.label = label
+        self.size = size
