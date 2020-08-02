280449a2b514285eeea301a108bb251c6504cf58
EastFull <huromech@gmail.com>
2020-08-02 16:47:23 +0900

    add models.py

---
+++ model_inference_restful/api/models.py
@@ -0,0 +1,7 @@
+class ImageModel:
+    def __init__(self, size, reasoning_time, predicted_label):
+        # We will automatically genera
+        self.id = 0
+        self.size = size
+        self.reasoning_time = reasoning_time
+        self.predicted_label = predicted_label
